d566a08bfcf4ab4343ba4447be29c84160bf2ef5
Ondrej Mirtes <ondrej@mirtes.cz>
2018-04-03 12:10:55 +0200

    Updated PHPStan

--- src/Reflection/Dibi/DibiFluentMethodReflection.php
+++ src/Reflection/Dibi/DibiFluentMethodReflection.php
@@ -2,6 +2,7 @@
 
 namespace PHPStan\Reflection\Dibi;
 
+use PHPStan\Reflection\ClassMemberReflection;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Type\ObjectType;
@@ -32,7 +33,7 @@ class DibiFluentMethodReflection implements MethodReflection
 		return $this->dibiFluent;
 	}
 
-	public function getPrototype(): MethodReflection
+	public function getPrototype(): ClassMemberReflection
 	{
 		return $this;
 	}
