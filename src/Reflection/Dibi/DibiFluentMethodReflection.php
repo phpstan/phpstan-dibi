80c582d7a4687e6d071d1fe70a12664b9f44555a
Ondrej Mirtes <ondrej@mirtes.cz>
2024-10-04 14:01:49 +0200

    Fix after PHPStan update

--- src/Reflection/Dibi/DibiFluentMethodReflection.php
+++ src/Reflection/Dibi/DibiFluentMethodReflection.php
@@ -6,7 +6,6 @@ use PHPStan\Reflection\ClassMemberReflection;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Reflection\FunctionVariant;
 use PHPStan\Reflection\MethodReflection;
-use PHPStan\Reflection\ParametersAcceptor;
 use PHPStan\TrinaryLogic;
 use PHPStan\Type\Generic\TemplateTypeMap;
 use PHPStan\Type\ObjectType;
@@ -55,9 +54,6 @@ class DibiFluentMethodReflection implements MethodReflection
 		return true;
 	}
 
-	/**
-	 * @return ParametersAcceptor[]
-	 */
 	public function getVariants(): array
 	{
 		return [
